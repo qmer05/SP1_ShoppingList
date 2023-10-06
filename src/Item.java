b666ff9d42e7f17e939699189bd795c5719ebf26
qmer05 <cph-oo203@cphbusiness.dk>
2023-10-06 12:40:08 +0200

    update

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Item {
+public class Item {
+    private String itemName;
+    private int itemQuantity;
+
+
+    public Item(String itemName, int itemQuantity){
+        this.itemName = itemName;
+        this.itemQuantity = itemQuantity;
+    }
+
+    public String getItemName(){
+        return itemName;
+    }
+
+    public int getItemQuantity(){
+        return itemQuantity;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Main {
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args){
+
+        ArrayList<Item> items = new ArrayList<>();
+        Item item1 = new Item("milk",3);
+        Item item2 = new Item("bread",2);
+        Item item3 = new Item("butter",4);
+        items.add(item1);
+        items.add(item2);
+        items.add(item3);
+
+        for (int i = 0; i < items.size(); i++){
+        System.out.println(items.get(i).getItemName() + items.get(i).getItemQuantity());
+        }
+    }
 }
