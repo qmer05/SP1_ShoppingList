bf3725d458535d905d20dc4d8da719c99b941366
qmer05 <cph-oo203@cphbusiness.dk>
2023-10-08 18:51:28 +0200

    update

--- src/Item.java
+++ src/Item.java
@@ -1,19 +1,41 @@
+import java.util.ArrayList;
+
 public class Item {
     private String itemName;
     private int itemQuantity;
 
-
-    public Item(String itemName, int itemQuantity){
+    public Item(String itemName, int itemQuantity) {
         this.itemName = itemName;
         this.itemQuantity = itemQuantity;
     }
 
-    public String getItemName(){
+    public String getItemName() {
         return itemName;
     }
 
-    public int getItemQuantity(){
+    public int getItemQuantity() {
         return itemQuantity;
     }
 
+    public void setItemName(String newItemName) {
+        this.itemName = newItemName;
+    }
+
+    public void setItemQuantity(int newItemQuantity) {
+        this.itemQuantity = newItemQuantity;
+    }
+
+    public void addItem(ArrayList<Item> newItemToAdd) {
+        for (Item item : newItemToAdd) {
+            if (item.itemName.equals(this.itemName)) {
+                System.out.println("This item is already on the list");
+                return;
+            }
+        }
+        newItemToAdd.add(this);
+    }
+
+    public String toString() {
+        return "Item: " + itemName + "\tQuantity: " + itemQuantity;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,22 @@
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         ArrayList<Item> items = new ArrayList<>();
-        Item item1 = new Item("milk",3);
-        Item item2 = new Item("bread",2);
-        Item item3 = new Item("butter",4);
-        items.add(item1);
-        items.add(item2);
-        items.add(item3);
 
-        for (int i = 0; i < items.size(); i++){
-        System.out.println(items.get(i).getItemName() + items.get(i).getItemQuantity());
+        Item item1 = new Item("milk", 2);
+        Item item2 = new Item("butter", 5);
+        Item item3 = new Item("bread", 1);
+        Item item4 = new Item("butter", 10);
+
+        item1.addItem(items);
+        item2.addItem(items);
+        item3.addItem(items);
+        item4.addItem(items);
+
+        for (Item i : items) {
+            System.out.println(i);
         }
     }
 }
